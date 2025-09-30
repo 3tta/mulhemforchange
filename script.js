48160eb9af5aa4a5d0e84cdd7fb24ba98397331e
Mohamed Atta <108631420+3tta@users.noreply.github.com>
2025-09-30 16:49:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
-});
+function toggleMenu() {
+  const menu = document.querySelector('.navbar ul');
+  menu.classList.toggle('show');
+}
