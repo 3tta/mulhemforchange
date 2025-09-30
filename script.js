acb5f86a10e779fd1ad0c081231bf2e4a8c26eaf
Mohamed Atta <108631420+3tta@users.noreply.github.com>
2025-09-30 16:56:43 +0300

    Refactor toggleMenu to use DOMContentLoaded event

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function toggleMenu() {
+document.addEventListener('DOMContentLoaded', function () {
+  const menuToggleBtn = document.querySelector('.menu-toggle');
   const menu = document.querySelector('.navbar ul');
-  menu.classList.toggle('show');
-}
+  if (menuToggleBtn && menu) {
+    menuToggleBtn.addEventListener('click', function () {
+      menu.classList.toggle('show');
+    });
+  }
+});
